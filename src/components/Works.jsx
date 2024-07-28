40d44dfcb8516923c6b702a2b0f361d6fa4ec7d5
Laila-front-dev <your.lailaben@gmail.com>
2024-07-28 13:58:52 +0100

    add cercle

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -3,6 +3,11 @@ function Works() {
   return (
     <>
       <h2 className="heading__2 clr-primary-400">How it works</h2>
+      <div className="line">
+        <span className="cercle"></span>
+        <span className="cercle"></span>
+        <span className="cercle"></span>
+      </div>
       <ServicesWork />
     </>
   );
